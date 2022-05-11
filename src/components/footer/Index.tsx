cb6c599eec1ad8613dc33891888cb94df1aa91fe
JaqKent <jaq.kent@gmail.com>
2022-05-10 21:38:21 -0300

    homePage

--- src/components/footer/Index.tsx
+++ src/components/footer/Index.tsx
@@ -6,22 +6,26 @@ import styles from "./styles.module.scss";
 function Footer() {
   return (
     <div className={styles.container}>
-      <div className={styles.follow}>
-        <h3>¡SEGUINOS EN LAS REDES!</h3>
-      </div>
-      <div className={styles.logo}>
-        <Facebook
-          width={45}
-          height={45}
-          fill="#fff"
-          onClick={() => console.log("facebook")}
-        />
-        <Instagram
-          width={45}
-          height={45}
-          fill="#fff"
-          onClick={() => console.log("Instagram")}
-        />
+      <div className={styles.sectionUp}>
+        <div className={styles.follow}>
+          <h3>¡SEGUINOS EN LAS REDES!</h3>
+        </div>
+        <div className={styles.logo}>
+          <Facebook
+            width={45}
+            height={45}
+            fill="#fff"
+            onClick={() => console.log("facebook")}
+          />
+          <p>Facebook</p>
+          <Instagram
+            width={45}
+            height={45}
+            fill="#fff"
+            onClick={() => console.log("Instagram")}
+          />
+          <p>Instagram</p>
+        </div>
       </div>
       <div className={styles.cuervo}>
         <p>Cuervo del Norte 2022</p>
