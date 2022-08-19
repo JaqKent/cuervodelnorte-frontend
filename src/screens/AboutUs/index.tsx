92b80278d49af2c29b7f396f6b605aaf41906ef5
JaqKent <jaq.kent@gmail.com>
2022-08-19 19:21:38 -0300

    product quantity in cart

--- src/screens/AboutUs/index.tsx
+++ src/screens/AboutUs/index.tsx
@@ -5,11 +5,9 @@ function AboutUs() {
     <div className={styles.container}>
       <h1>Somos Cuervo del Norte </h1>
       <h3>
-        Cuervo del norte es una marroquineria ubicada en tucuman argentina que
-        trabaja con materioales de la mejor calidad, brindando productos
-        excelentes y duraderos.
+        Un emprendimiento Tucumano. Fabricamos una amplia linea de productos de
+        marroquineria urbana. Somos una marca independiente, 100% Tucumana.
       </h3>
-      <p>Jose Maria Frias</p>
     </div>
   );
 }
