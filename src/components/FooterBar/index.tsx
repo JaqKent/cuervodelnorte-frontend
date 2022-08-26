9a81835ff2bd03cdfb9c3336fe8cc761ad93473d
JaqKent <jaq.kent@gmail.com>
2022-08-26 20:43:54 -0300

    changess

--- src/components/FooterBar/index.tsx
+++ src/components/FooterBar/index.tsx
@@ -1,7 +1,8 @@
-import { ReactComponent as Facebook } from "assets/facebook.svg";
-import { ReactComponent as Instagram } from "assets/instagram.svg";
+import { ReactComponent as Facebook } from 'assets/facebook.svg';
+import { ReactComponent as Instagram } from 'assets/instagram.svg';
+import { ReactComponent as Whatsapp } from 'assets/whatsapp.svg';
 
-import styles from "./styles.module.scss";
+import styles from './styles.module.scss';
 
 function FooterBar() {
   return (
@@ -12,34 +13,47 @@ function FooterBar() {
         </div>
         <div className={styles.logo}>
           <a
-            target="_blank"
-            href="https://www.facebook.com/Cuervo-del-Norte-102097179028369/"
-            rel="noreferrer"
+            target='_blank'
+            href='https://www.facebook.com/Cuervo-del-Norte-102097179028369/'
+            rel='noreferrer'
           >
-            <Facebook width={45} height={45} fill="#fff" />
+            <Facebook width={45} height={45} fill='#fff' />
           </a>
           <p className={styles.links}>
-            {" "}
+            {' '}
             <a
               className={styles.links}
-              target="_blank"
-              href="https://www.facebook.com/Cuervo-del-Norte-102097179028369/"
-              rel="noreferrer"
+              target='_blank'
+              href='https://www.facebook.com/Cuervo-del-Norte-102097179028369/'
+              rel='noreferrer'
             >
               Facebook
             </a>
           </p>
-          <a href="http://www.instagram.com/cuervodelnorte.tuc">
-            <Instagram width={45} height={45} fill="#fff" />
+          <a href='http://www.instagram.com/cuervodelnorte.tuc'>
+            <Instagram width={45} height={45} fill='#fff' />
           </a>
 
           <p className={styles.links}>
             <a
-              target="_blank"
-              href="http://www.instagram.com/cuervodelnorte.tuc"
-              rel="noreferrer"
+              target='_blank'
+              href='http://www.instagram.com/cuervodelnorte.tuc'
+              rel='noreferrer'
             >
               Instagram
+            </a>
+          </p>
+          <a href={process.env.REACT_APP_WHATSAPP}>
+            <Whatsapp width={45} height={45} fill='#fff' />
+          </a>
+
+          <p className={styles.links}>
+            <a
+              target='_blank'
+              href={process.env.REACT_APP_WHATSAPP}
+              rel='noreferrer'
+            >
+              Whatsapp
             </a>
           </p>
         </div>
