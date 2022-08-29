6b709f6935e6ab51c66838ba3a0ca92964ef5acd
JaqKent <jaq.kent@gmail.com>
2022-08-29 19:15:06 -0300

    final-changes

--- src/screens/LandingPage/constants.ts
+++ src/screens/LandingPage/constants.ts
@@ -1,22 +1,23 @@
 import tuc from 'assets/tucuman.svg';
+import delivery from 'assets/delivery.svg';
 
 export const COMMENT = [
   {
     id: 1,
     imagen: tuc,
-    text1: 'Hecho en Tucumán',
-    text2: 'En el corazon del Norte Argentino',
-  },
-  {
-    id: 1,
-    imagen: tuc,
-    text1: 'Somos una marca Independiente',
-    text2: 'una marca 100% Tucumana',
+    text1: ' 100% Hecho en Tucumán',
+    text2: 'En el corazon del Norte Argentino.',
   },
   {
     id: 1,
     imagen: tuc,
     text1: 'Somos Fabricantes',
     text2: 'Tenemos una amplia linea de productos de marroquineria urbana',
+  },
+  {
+    id: 1,
+    imagen: delivery,
+    text1: 'ENVIOS PROGRAMADOS SIN CARGO SAN MIGUEL DE TUCUMÁN Y ALREDEDORES',
+    text2: '',
   },
 ];
